71698e29f92877ae7b4833e92d68ffe35fc9a5c9
Wellington Leardini <wellington-158@hotmail.com>
2022-11-28 22:09:02 -0300

    fix: import dinâmico

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,6 @@
+const path = require("path");
+const { mergeConfig } = require("vite");
+
 module.exports = {
 	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
@@ -5,18 +8,23 @@ module.exports = {
 		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
 		"@storybook/addon-a11y",
+		"@storybook/addon-interactions",
 	],
 	framework: "@storybook/react",
-	core: {
-		builder: "@storybook/builder-vite",
-	},
+	core: { builder: "@storybook/builder-vite" },
 	features: {
 		storyStoreV7: true,
+		interactionsDebugger: true,
 	},
 	viteFinal: (config, { configType }) => {
 		if (configType === "PRODUCTION") {
 			config.base = "/design-system/";
 		}
-		return config;
+		return mergeConfig(config, {
+			// customize the Vite config here
+			resolve: {
+				alias: [{ find: "~", replacement: path.resolve(__dirname, "..", "src") }],
+			},
+		});
 	},
 };
