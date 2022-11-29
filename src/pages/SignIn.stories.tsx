e4087a2ffe5230de9757f99387b9c50c094196bd
Wellington Leardini <wellington-158@hotmail.com>
2022-11-28 22:08:29 -0300

    refactor: organixzação

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from "./SignIn";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+	title: "Pages/Sign In",
+	component: SignIn,
+	args: {},
+	argTypes: {},
+} as Meta;
+
+export const Default: StoryObj = {};
+
+export const WithoutIcon: StoryObj = {};
